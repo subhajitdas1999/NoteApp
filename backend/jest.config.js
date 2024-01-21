fcb8a15588de79987072bb791c138c75f8fe5730
subhajitdas1999 <sd651846@gmail.com>
2024-01-22 00:07:11 +0530

    added some customisations,path aliases

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,16 +1,19 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  // [...]
-  transform: {
-    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
-    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/**/*.test.ts"],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true, //Automatically clear mock calls, instances, contexts and results before every test. Equivalent to calling jest.clearAllMocks() before each test. This does not remove any mock implementation that may have been provided.
   moduleNameMapper: {
-    "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
-    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
     "^@models/(.*)$": "<rootDir>/src/models/$1",
     "^@routes/(.*)$": "<rootDir>/src/routes/$1",
+    "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
+    "^@middlewares/(.*)$": "<rootDir>/src/middlewares/$1",
     "^@services/(.*)$": "<rootDir>/src/services/$1",
     "^@validators/(.*)$": "<rootDir>/src/validators/$1",
   },
