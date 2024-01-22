a5a112ce9fa5f33e856070e3c76024cd1cb5f4b5
subhajitdas1999 <sd651846@gmail.com>
2024-01-23 01:45:19 +0530

    added rate limit middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,20 @@ import authRouter from "@routes/auth.routes";
 import notesRouter from "@routes/notes.routes";
 import { globalErrorHandler } from "@services/error.service";
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 app.use(express.json());
+
+//limit middleware to stop to many request from a certain IP to the server
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 50, // Limit each IP to 50 requests per `window` (here, per 5 minutes).
+  message: "To many requests from this IP. Please try again after some time",
+});
+
+//Apply the rate limiting middleware to API calls only
+app.use("/api", limiter);
 
 app.use("/api/auth", authRouter);
 app.use("/api/notes", notesRouter);
